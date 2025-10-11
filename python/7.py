81578f621a6a24d7db31f889c0b20a1f726d3818
witi <wblalock93@icloud.com>
2025-10-11 00:05:13 -0400

    finished 7

---
+++ python/7.py
@@ -0,0 +1,23 @@
+def nth_prime_number(n):
+    prime_count = 1
+    numb = 1
+    while prime_count != n:
+        if prime(numb):
+            prime_count += 1
+            nth_prime = numb
+            numb += 1
+        else:
+            numb += 1
+            
+    return nth_prime
+
+def prime(n):
+    is_prime = True
+    numb = 2
+    while numb < n:
+        if n % numb == 0:
+            is_prime = False
+            break
+        else:
+            numb += 1
+    return is_prime
